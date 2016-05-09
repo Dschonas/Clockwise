b28fd2e9d55d9d4f9436e646d6d9ff2097f532d1
langhaarzombie <mixmax199989@googlemail.com>
2016-05-09 20:08:28 +0200

    Reworkd enire webiste (backup was made), there was too much percent and absolute, Added: First section, sectiond section, nearly finished, a bit responsive

--- website/js/style.js
+++
@@ -1,6 +0,0 @@
-$('#registerform input').hasClass('textinput').blur(function()          //whenever you click off an input element
-{                   
-    if( !$(this).val() ) {                      //if it is blank. 
-         alert('empty');    
-    }
-});
